c75ea93fcef4d4f00111b5530b9ff6ac875493dc
zamarz <zachmarzouk@gmail.com>
2024-02-28 11:11:58 +0000

    Add theme to layout and alter tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +9,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#012a4a",
+        secondary: "#a9d6e5",
+        tertiary: {
+          dark: "#231942",
+          light: "#a9d6e5",
+        },
       },
     },
   },
