bad3fdc937368b74733d554f1dcc229cd55da040
zamarz <zachmarzouk@gmail.com>
2024-03-06 11:43:29 +0000

    Create regex for number search in job description

--- src/components/MPChildInterestAnalysis.tsx
+++ src/components/MPChildInterestAnalysis.tsx
@@ -1,9 +1,18 @@
 const MPChildInterestAnalysis = ({ childInterestInfo }: any) => {
   console.log(childInterestInfo, "childinterestinfo");
 
+  const regexAmount = /£([\d,]+)/g;
+  const priceEarnings = childInterestInfo.interest.match(regexAmount);
+  const removePound = priceEarnings[0].slice(1, priceEarnings[0].length);
+
+  console.log(priceEarnings);
+  console.log(removePound, "poundremove");
+
   return (
     <div>
-      <div>{childInterestInfo.interest}</div>
+      <div className="py-2 mx-auto">
+        <div>{childInterestInfo.interest}</div>
+      </div>
     </div>
   );
 };
--- src/components/MPEmploymentEarnings.tsx
+++ src/components/MPEmploymentEarnings.tsx
@@ -1,14 +1,23 @@
+"use client";
+import { useState } from "react";
 import MPChildInterestAnalysis from "./MPChildInterestAnalysis";
 
 const MPEmploymentEarnings = ({ earningsInfo }: any) => {
+  const [earnings, setEarnings] = useState(0);
+  const [hours, setHours] = useState(0);
+
   return (
     <div>
-      <h4 className="font-bold text-xl">{earningsInfo.name}</h4>
-      <div>
+      <h4 className="font-bold text-xl py-3 mx-auto text-center">
+        {earningsInfo.name}
+      </h4>
+      <div className="text-center py-3 mx-auto">
         {earningsInfo.interests.map((secondJob: any) => {
           return (
             <div>
-              <h3>{secondJob.interest}</h3>
+              <h3 className="font-semibold mx-auto py-3">
+                {secondJob.interest}
+              </h3>
 
               {secondJob.childInterests.length > 0 ? (
                 secondJob.childInterests.map((childInterest: any) => {
@@ -22,9 +31,15 @@ const MPEmploymentEarnings = ({ earningsInfo }: any) => {
               ) : (
                 <></>
               )}
+              <div> Earnings: 0</div>
+              <div> Hours: 0</div>
             </div>
           );
         })}
+        <div className="text-xl py-5">
+          <div>Total Earnings: 0</div>
+          <div>Total Hours: 0</div>
+        </div>
       </div>
     </div>
   );
