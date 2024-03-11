4e330f22f29f8474aeeeb3d229ad8ecc6283f16c
zamarz <zachmarzouk@gmail.com>
2024-03-11 08:57:34 +0000

    Alter child interest styling

--- src/components/MPChildInterestAnalysis.tsx
+++ src/components/MPChildInterestAnalysis.tsx
@@ -22,16 +22,19 @@ const MPChildInterestAnalysis = ({ childInterestInfo }: any) => {
 
   earningsAggregator(childInterestInfo, childInterestEarningsArray);
   hoursAggregator(childInterestInfo, childInterestHoursArray);
-
+  // grid grid-cols-2 lg:grid-cols-3 gap-4
   return (
-    <div className="mx-auto w-1/3">
-      <div className="py-2 mx-auto text-center text-xl text-balance">
-        <div className="py-2 mx-auto">{childInterestInfo.interest}</div>
-        <div className="py-2 mx-auto">
-          <span className="font-bold">Earnings:</span> £{childInterestEarnings}
-        </div>
-        <div className="py-2 mx-auto">
-          <span className="font-bold">Hours:</span> {childInterestHours}
+    <div className="mx-auto w-full md:w-2/3 lg:w-2/3 grid grid-cols-3 gap-4">
+      <div className=" ">
+        <div className="py-2 mx-auto text-center text-xl text-balance">
+          <div className="py-2 mx-auto">{childInterestInfo.interest}</div>
+          <div className="py-2 mx-auto">
+            <span className="font-bold">Earnings:</span> £
+            {childInterestEarnings}
+          </div>
+          <div className="py-2 mx-auto">
+            <span className="font-bold">Hours:</span> {childInterestHours}
+          </div>
         </div>
       </div>
     </div>
