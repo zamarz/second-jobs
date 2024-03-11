682fb8b1a47d5a56f66fe592e18f2af213ff0206
zamarz <zachmarzouk@gmail.com>
2024-03-11 10:11:05 +0000

    Add more styling to search component

--- src/components/MPCard.tsx
+++ src/components/MPCard.tsx
@@ -8,7 +8,7 @@ const MPCard = ({ mpInfo }: any) => {
   }
 
   return (
-    <div className="py-3 mx-auto">
+    <div className="py-3 mx-auto lg:mx-3">
       <div className="items-center text-center py-3 mx-auto">
         <Image
           src={mpInfo.thumbnailUrl}
@@ -17,7 +17,7 @@ const MPCard = ({ mpInfo }: any) => {
           width={200}
         />
         <h3>{mpInfo.nameDisplayAs}</h3>
-        <p>Party: {mpInfo.latestParty.name}</p>
+        <p>{mpInfo.latestParty.name}</p>
         <button className="button">
           <Link href={`/mps/${mpInfo.id}`}>See Expenses</Link>
         </button>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { getMPSearch } from "@/app/utils/api";
-import { error } from "console";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import MPCard from "./MPCard";
 
 const Search = () => {
@@ -25,18 +24,21 @@ const Search = () => {
   return (
     <section>
       <div>
-        <label className="mx-3">MP Name:</label>
+        <label className="mx-3 text-2xl">Search for an MP: </label>
         <input
           className="outline"
           placeholder="Search for an MP here..."
           id="userSearch"
           onChange={(event) => setSearchTerm(event.target.value)}
         ></input>
-        <button className="mx-6 button" type="submit">
+        <button className="mx-6 button" type="button">
           Search
         </button>
       </div>
-      <div hidden={false}>
+      <div
+        hidden={false}
+        className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 mx-3 py-3"
+      >
         {Object.keys(searchAPIData).length > 0 ? (
           searchAPIData.items.map((mp: any) => {
             return <MPCard key={mp.value.id} mpInfo={mp.value} />;
