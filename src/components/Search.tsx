bb8a927d3d67913bfe24d644a88c1017df3f3865
zamarz <zachmarzouk@gmail.com>
2024-03-11 09:25:50 +0000

    Create search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,28 @@
+"use client";
+
+import { ChangeEvent, useEffect, useState } from "react";
+
 const Search = () => {
-  return <div>Search goes here...</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {}, [searchTerm]);
+
+  return (
+    <section>
+      <div>
+        <label className="mx-3">MP Name:</label>
+        <input
+          className="outline"
+          placeholder="Search for an MP here..."
+          id="userSearch"
+          onChange={(event) => setSearchTerm(event.target.value)}
+        ></input>
+        <button className="mx-6 button" type="submit">
+          Search
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Search;
