f9c9b84a4db6af141c92d6f76197019bb6b85a3e
zamarz <zachmarzouk@gmail.com>
2024-03-15 10:03:31 +0000

    Add icons to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { MdFormatListBulleted, MdOutlineEmail } from "react-icons/md";
+import { RiContactsBook2Line } from "react-icons/ri";
 
 const Footer = () => {
+  const style = { fontSize: "1.5em" };
   return (
     <footer className="bg-primary text-white py-8 px-6 sm:px-8 lg:px-16">
       <div className="container mx-auto max-w-screen-lg xl:max-w-screen-xl">
@@ -8,18 +11,21 @@ const Footer = () => {
           <Link href="/" className="font-black text-tertiary-light">
             MP Second Jobs
           </Link>
-          <div className="space-y-2">
-            <h4 className="font-semibold mb-4">
+          <div className="space-y-2 flex items-center">
+            <RiContactsBook2Line style={style} />
+            <h4 className="font-semibold mb-4 px-3">
               <Link href="/about">About</Link>
             </h4>
           </div>
-          <div className="space-y-2">
-            <h4 className="font-semibold mb-4">
+          <div className="space-y-2 flex items-center">
+            <MdFormatListBulleted style={style} />
+            <h4 className="font-semibold mb-4 px-3">
               <Link href="/mps">MP List</Link>
             </h4>
           </div>
-          <div className="space-y-2">
-            <h4 className="font-semibold mb-4">
+          <div className="space-y-2 flex items-center">
+            <MdOutlineEmail style={style} />
+            <h4 className="font-semibold mb-4 px-3">
               <Link href="/contact">Contact Me</Link>
             </h4>
           </div>
