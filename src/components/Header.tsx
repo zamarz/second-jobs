8a1d51cce61b517c6b009776e504991c5e1606ff
zamarz <zachmarzouk@gmail.com>
2024-03-20 11:29:52 +0000

    Alter header size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,14 +12,14 @@ const Header = () => {
 
   return (
     <header className="bg-primary text-white py-8 px-6 text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
-      <div className="flex mx-9 my-4 items-center w-full md:w-2/3">
+      <div className="flex mx-9 my-4 items-center w-full md:w-1/3 md:items-center md:justify-center">
         <Link
           href="/"
-          className="font-black text-tertiary-light mx-3 md:align-center"
+          className="font-black text-tertiary-light ml-3 md:align-center"
         >
           MP Second Jobs
         </Link>
-        <ul className="flex items-center ml-5">
+        <ul className="flex items-center justify-center ml-5">
           <li className="ml-2">
             {darkTheme ? (
               <HiLightBulb
@@ -41,7 +41,7 @@ const Header = () => {
           </li>
         </ul>
       </div>
-      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4 mx-12">
+      <ul className="flex items-center justify-around w-full md:w-1/3 mt-4 mr-12">
         <li className="hover:-translate-y-2 duration-500 transition-all">
           <Link href="/about" className="text-sm md:text-base lg:text-xl">
             About
